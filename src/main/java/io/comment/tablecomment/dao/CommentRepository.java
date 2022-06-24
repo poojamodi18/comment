86801e30080bc147f91798176f833457f0eb6e7d
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-06-24 14:50:31 +0530

    removed unused imports

--- src/main/java/io/comment/tablecomment/dao/CommentRepository.java
+++ src/main/java/io/comment/tablecomment/dao/CommentRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
