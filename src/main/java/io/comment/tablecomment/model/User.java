d8ff4e5cde7412ffd196563e85e51abffbbb1270
Pooja Modi <40056158+poojamodi18@users.noreply.github.com>
2022-07-04 10:31:36 +0530

    user entity

--- src/main/java/io/comment/tablecomment/model/User.java
+++ src/main/java/io/comment/tablecomment/model/User.java
@@ -1,10 +1,10 @@
 package io.comment.tablecomment.model;
 
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -12,7 +12,10 @@ import javax.persistence.Id;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String userName;
     private String password;
+    @Embedded
+    private Address address;
 }
